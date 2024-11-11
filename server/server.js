c4fccc7f412ba17ef912d6d4c59036c7d66e67d5
Danielius <danieliusryliskis.dev@gmail.com>
2024-11-11 16:30:20 +0200

    m

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-  res.send("server is running");
+  res.send("server is rauaanning");
 });
 
 app.listen(5000, console.log("on 5000"));

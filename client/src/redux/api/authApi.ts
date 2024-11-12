3456153a6213afc5f2fd2d41bda99e2b30533542
Danielius <danieliusryliskis.dev@gmail.com>
2024-11-12 17:33:28 +0200

    a

--- client/src/redux/api/authApi.ts
+++ client/src/redux/api/authApi.ts
@@ -3,27 +3,27 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://e-commerce-server289.vercel.app",
+    baseUrl: "https://e-commerce-server289.vercel.app/",
     credentials: "include",
   }),
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (userCredentials) => ({
-        url: "/api/auth/login",
+        url: "api/auth/login",
         method: "POST",
         body: userCredentials,
       }),
     }),
     register: builder.mutation({
       query: (newUser) => ({
-        url: "/api/auth/register",
+        url: "api/auth/register",
         method: "POST",
         body: newUser,
       }),
     }),
     logout: builder.mutation({
       query: () => ({
-        url: "/api/auth/logout",
+        url: "api/auth/logout",
         method: "POST",
       }),
     }),

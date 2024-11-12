a6addcc82e7e734e3030df0f97e4cd896acf310c
Danielius <danieliusryliskis.dev@gmail.com>
2024-11-12 12:23:12 +0200

    a

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel v2 test"));
+app.get("/", (req, res) => res.send("Express on Vercel v2 testfff"));
 
 app.listen(3001, () => console.log("Server ready on port 3000."));
 
